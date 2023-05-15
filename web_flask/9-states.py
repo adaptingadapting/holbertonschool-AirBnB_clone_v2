f2c0685cc3521fdb625a371b124197c77cf87372
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-05-15 02:29:04 -0300

    task 9

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -14,6 +14,7 @@ def states_list():
     sorted_states = sorted(states, key=lambda state: state.name)
     return render_template('7-states_list.html', states=sorted_states)
 
+
 @app.route('/states/<id>')
 def state_cities(id):
     state = storage.get(State, id)
@@ -22,9 +23,11 @@ def state_cities(id):
     cities = sorted(state.cities, key=lambda city: city.name)
     return render_template('9-states.html', state=state, cities=cities)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
