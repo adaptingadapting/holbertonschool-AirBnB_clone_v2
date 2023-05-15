38a30abd2bdb23ba454769aa6ada8d89ff948129
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-05-15 00:15:32 -0300

    task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -28,10 +28,9 @@ def pythoniscool(text="is cool"):
     return "Python" + " " + text.replace("_", " ")
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def display_int(n):
-    if type(n) is int:
-        return f"{n} is a number"
+    return f"{n} is a number"
 
 
 if __name__ == '__main__':
