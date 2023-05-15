f2d03fb4081c78ce34b8ff90596e964dd55b0597
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-05-15 02:07:09 -0300

    task 8

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slash=False)
+def cities_by_states():
+    states_dict = storage.all(State)
+    dict = {}
+    for val in states_dict.values():
+        dict[val] = val.cities
+    return render_template('8-cities_by_states.html', dict=dict)
+
+
+@app.teardown_appcontext
+def close(exception):
+    """Close database connection"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
